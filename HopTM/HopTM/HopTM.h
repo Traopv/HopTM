0ef07bb7a410274d56b7530983d04ec69397439b
Your Nametraopv <traopv99@gmail.com>
2021-01-29 15:20:30 +0700

    Tao framework cho lich hopTM

---
+++ HopTM/HopTM/HopTM.h
@@ -0,0 +1,18 @@
+//
+//  HopTM.h
+//  HopTM
+//
+//  Created by Apple on 1/29/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for HopTM.
+FOUNDATION_EXPORT double HopTMVersionNumber;
+
+//! Project version string for HopTM.
+FOUNDATION_EXPORT const unsigned char HopTMVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HopTM/PublicHeader.h>
+
+
---
+++ HopTM/HopTM/Service.swift
@@ -0,0 +1,16 @@
+//
+//  Service.swift
+//  HopTM
+//
+//  Created by Apple on 1/29/21.
+//
+
+import Foundation
+
+public class Service {
+    public init() {}
+    
+    public static func doSomething() -> String {
+        return "cocoapod cho lich hop thong minh"
+    }
+}
